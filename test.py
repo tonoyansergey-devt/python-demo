8b9d62b2bcc43b9b8c578247cfe214daed2898cf
tonoyansergey-devt <sergey.tonoyan@devtactics.net>
2025-02-25 17:05:42 +0400

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import os
 
 print('The applied environment variable value is:')
 print(os.environ.get('MY_VAR3'))
+print(os.environ.get('ENV_FROM_FORK'))
+
 
 if os.environ.get('MY_VAR3') == 'TEST':
     print('success')
