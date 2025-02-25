e3de08e5d96fc7f20635d47cd555fefee503c625
tonoiansergey <tonoiansergey@gmail.com>
2025-02-25 16:51:20 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import os
 
-print('the applied env vasww env is')
+print('The applied environment variable value is:')
 print(os.environ.get('MY_VAR3'))
+
+if os.environ.get('MY_VAR3') == 'TEST':
+    print('success')
+else:
+    print('failure')
